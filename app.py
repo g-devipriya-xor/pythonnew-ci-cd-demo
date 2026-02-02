6c674009d9cf6a932f8a6ed5f118e709ae91d544
Devipriya <G.Devipriya@xoriant>
2026-02-02 10:51:30 +0000

    change

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Python CI/CD Pipeline updated test"
+    return "Hello from Python CI/CD Pipeline updated test PR"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001)
