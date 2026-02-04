62ee5bfcb716e0d0e7dfd3f080c7da5e5e1592a7
Devipriya <G.Devipriya@xoriant>
2026-02-04 06:20:57 +0000

    app

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-	return "Hello from Python CI/CD Pipeline updated test"
+	return "Hello from Python CI/CD Pipeline updated test two"
 @app.route("/status")
 def status():
     return "App is running"
