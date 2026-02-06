c02dd95766a5f0fed2495708a0630c5866f43af5
Devipriya <G.Devipriya@xoriant>
2026-02-06 04:48:48 +0000

    app.py

--- app.py
+++ app.py
@@ -7,6 +7,6 @@ def hello():
 	return "Hello from Python CI/CD Pipeline updated test four"
 @app.route("/status")
 def status():
-    return "App is running"
+    return "App is running new-branch"
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001)
